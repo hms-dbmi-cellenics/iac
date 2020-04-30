0e4cd56d8d5d7c9bbe8d51fa56cf1244cc0af4ef
Marcell Pék <marcell.pek@marcellp.net>
2020-04-30 13:49:51 +0100

    fix

--- src/api/routes/health.js
+++ src/api/routes/health.js
@@ -1,8 +1,11 @@
+const config = require('./config');
+
 module.exports = {
   'health#check': (req, res) => {
     res.json({
       status: 'up',
       env: process.env.NODE_ENV,
+      clusterEnv: config.clusterEnv,
     });
   },
 };
