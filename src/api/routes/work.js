eeaecc1afcda8967e9a11cbeb3b355a79d2dce7f
Marcell Pék <marcell.pek@marcellp.net>
2020-04-30 20:50:12 +0100

    fix url

--- src/api/routes/work.js
+++ src/api/routes/work.js
@@ -1,6 +1,9 @@
-const snsMiddleware = require('express-sns-validator');
+const https = require('https');
+
+const MessageValidator = require('sns-validator');
 const WorkService = require('../../services/work');
 
+const validator = new MessageValidator();
 
 module.exports = {
   'work#submit': (req, res) => {
@@ -10,7 +13,29 @@ module.exports = {
     res.json({ wow: 'hi from work' });
   },
   'work#receive': [(req, res) => {
-    console.log(req);
-    res.status(200).json({});
+    let msg;
+
+    try {
+      msg = JSON.parse(req.body);
+    } catch (error) {
+      res.status(500).body('nok');
+    }
+
+    validator.validate(msg, (err, message) => {
+      if (err) {
+        return;
+      }
+
+      if (message.Type === 'SubscriptionConfirmation'
+        || message.Type === 'UnsubscribeConfirmation') {
+        https.get(message.SubscribeURL);
+      }
+
+      if (message.Type === 'Notification') {
+        console.log(message);
+      }
+    });
+
+    res.status(200).body('ok');
   }],
 };
