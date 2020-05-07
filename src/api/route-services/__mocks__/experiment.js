c11e4b717b4c566372c7d53c1eb0260e43f7d310
iva babukova <ibabukova@gmail.com>
2020-05-07 10:04:18 +0100

    unit tests for the experiment route

--- src/api/route-services/__mocks__/experiment.js
+++ src/api/route-services/__mocks__/experiment.js
@@ -7,7 +7,38 @@ const mockExperimentData = jest.fn((x) => new Promise((resolve, reject) => {
   });
 }));
 
+const mockGetCellSets = jest.fn((x) => new Promise((resolve, reject) => {
+  resolve({
+    cellSets: [
+      {
+        color: 'white',
+        name: 'Cell types',
+        key: 1,
+        children: [
+          {
+            color: 'blue',
+            key: 7,
+            name: 'some cells',
+          },
+          {
+            color: 'red',
+            key: 8,
+            name: 'some other cells',
+          },
+        ],
+      },
+      {
+        color: 'black',
+        name: 'amazing cells',
+        key: 3,
+      },
+    ],
+  });
+}));
 
-const mock = jest.fn().mockImplementation(() => ({ getExperimentData: mockExperimentData }));
+const mock = jest.fn().mockImplementation(() => ({
+  getExperimentData: mockExperimentData,
+  getCellSets: mockGetCellSets,
+}));
 
 module.exports = mock;
--- src/api/routes/experiment-routes.test.js
+++ src/api/routes/experiment-routes.test.js
@@ -12,22 +12,35 @@ describe('tests for experiment route', () => {
 
     request(app)
       .get('/v1/experiments/someId')
-      // .expect(200)
+      .expect(200)
       .end((err, res) => {
         if (err) {
           return done(err);
         }
-        const respKeys = Object.keys(res.body).sort();
-        expect(respKeys).toEqual(['experimentId', 'experimentName']);
         expect(res.body.experimentId).toBe('someId');
         expect(res.body.experimentName).toBe('my mocky name');
         return done();
       });
   });
 
+  it('Find cell sets by experiment id works', async (done) => {
+    const { app } = await expressLoader(express());
+    request(app)
+      .get('/v1/experiments/someId/cellSets')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
   afterEach(() => {
     /**
-     * Most important since b'coz of caching, the mocked implementations sometimes does not resets
+     * Most important since b'coz of caching, the mocked implementations sometimes does not reset
      */
     jest.resetModules();
     jest.restoreAllMocks();
