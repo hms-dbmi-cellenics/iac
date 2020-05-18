f620c28be82051ae698001ebb43376ab8a28460e
Marcell Pék <marcell.pek@marcellp.net>
2020-05-18 12:46:54 +0100

    simplify clusters

--- src/api/route-services/experiment.js
+++ src/api/route-services/experiment.js
@@ -11,26 +11,6 @@ class ExperimentService {
       matrixPath: 'julian-sep-19-intermediate-data/tgfb1-4-control.h5ad',
       cellSets: [
         {
-          key: 1,
-          name: 'Cell types',
-          rootNode: true,
-          children: [
-            { key: 7, name: 'Hepatocytes', color: '#008DA6' },
-            { key: 3, name: 'B cells', color: '#AB149E' },
-            { key: 4, name: 'Kupffer cells', color: '#F44E3B' },
-            {
-              key: 5,
-              name: 'Stellate cells and myofibroblasts',
-              color: '#FCDC00',
-            },
-            {
-              key: 6,
-              name: 'Liver sinusoidal endothelial cells',
-              color: '#68BC00',
-            },
-          ],
-        },
-        {
           key: 2,
           name: 'Louvain clusters',
           rootNode: true,
@@ -1551,16 +1531,6 @@ class ExperimentService {
           ],
         },
         { key: 15, name: 'My Custom Set', rootNode: true },
-        {
-          key: 11,
-          name: 'k-Means clusters',
-          rootNode: true,
-          children: [
-            { key: 12, name: 'Cluster 1', color: '#CCCCCC' },
-            { key: 13, name: 'Cluster 2', color: '#9F0500' },
-            { key: 14, name: 'Cluster 3', color: '#C45100' },
-          ],
-        },
       ],
     });
   }
