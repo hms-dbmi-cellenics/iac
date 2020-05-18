31ae75e77428282ed5aca9ef8a59c57bbe0307f1
Marcell Pék <marcell.pek@marcellp.net>
2020-05-18 16:32:19 +0100

    fix

--- src/api/route-services/experiment.js
+++ src/api/route-services/experiment.js
@@ -1580,7 +1580,7 @@ class ExperimentService {
 
     key = AWS.DynamoDB.Converter.marshall(key);
 
-    const data = AWS.DynamoDB.Converter.marshall(cellSetData);
+    const data = AWS.DynamoDB.Converter.marshall({ cellSetData });
 
     console.log(data);
 
