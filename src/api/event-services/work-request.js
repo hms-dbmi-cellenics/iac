51d5ffeb7e6188f67119ededbe76df73936f5588
Iva Babukova <iva@biomage.net>
2020-08-16 09:29:59 +0000

    Improvement of logic and add a little more logging in order to debug broken pagination

--- src/api/event-services/work-request.js
+++ src/api/event-services/work-request.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const WorkSubmitService = require('../general-services/work-submit');
 const logger = require('../../utils/logging');
-const { cacheGetRequest } = require('../../utils/cache-request');
+const { cacheGetRequest, CacheMissError } = require('../../utils/cache-request');
 const { handlePagination } = require('../../utils/handlePagination');
 
 const validateRequest = (workRequest) => {
@@ -28,23 +28,29 @@ const handleWorkRequest = async (workRequest, socket) => {
     logger.log(`Trying to fetch response to request ${uuid} from cache...`);
     const cachedResponse = await cacheGetRequest(workRequest);
     logger.log(`We found a cached response for ${uuid}. Checking if pagination is needed...`);
-
     if (pagination) {
       logger.log('Pagination is needed, processing response...');
       cachedResponse.results = handlePagination(cachedResponse.results, pagination);
       logger.log('Paginated');
+    } else {
+      logger.log('No pagination required.');
     }
     socket.emit(`WorkResponse-${uuid}`, cachedResponse);
     logger.log(`Response sent back to ${uuid}`);
   } catch (e) {
-    logger.log(`Cache miss on ${uuid}, sending it to the worker... ${e}`);
-    validateRequest(workRequest);
-    const { timeout } = workRequest;
-    if (Date.parse(timeout) <= Date.now()) {
-      throw new Error(`Work request will not be handled as timeout of ${timeout} is in the past...`);
+    if (e instanceof CacheMissError) {
+      logger.log(`Cache miss on ${uuid}, sending it to the worker... ${e}`);
+      validateRequest(workRequest);
+      const { timeout } = workRequest;
+      if (Date.parse(timeout) <= Date.now()) {
+        throw new Error(`Work request will not be handled as timeout of ${timeout} is in the past...`);
+      }
+      const workSubmitService = new WorkSubmitService(workRequest);
+      await workSubmitService.submitWork();
+    } else {
+      logger.log('Unexpected error happened while trying to process cached response.');
+      logger.trace(e);
     }
-    const workSubmitService = new WorkSubmitService(workRequest);
-    await workSubmitService.submitWork();
   }
 };
 
