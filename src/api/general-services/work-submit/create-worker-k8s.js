cab2c80e2ed7c59ebdd495e4475f9cc1c1d8d297
Marcell Pek <marcell.pek@marcellp.net>
2020-10-05 10:32:56 +0100

    fix

---
+++ src/api/general-services/work-submit/create-worker-k8s.js
@@ -0,0 +1,92 @@
+const k8s = require('@kubernetes/client-node');
+const config = require('../../../config');
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const createWorkerResources = async (service) => {
+  const namespaceName = 'worker-refs-heads-master';
+  const k8sBatchApi = kc.makeApiClient(k8s.BatchV1Api);
+  const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api);
+  const accountId = await config.awsAccountIdPromise();
+  const imageUrl = `${accountId}.dkr.ecr.${config.awsRegion}.amazonaws.com/worker:refs-heads-master-latest`;
+
+  await k8sCoreApi.createNamespacedPersistentVolumeClaim(namespaceName, {
+    apiVersion: 'v1',
+    kind: 'PersistentVolumeClaim',
+    metadata: {
+      name: `job-${service.workerHash}`,
+    },
+    spec: {
+      accessModes: [
+        'ReadWriteOnce',
+      ],
+      storageClassName: 'worker-storage',
+      resources: {
+        requests: {
+          storage: '10Gi',
+        },
+      },
+    },
+  });
+
+  await k8sBatchApi.createNamespacedJob(namespaceName, {
+    apiVersion: 'batch/v1',
+    kind: 'Job',
+    metadata: {
+      name: `job-${service.workerHash}`,
+      labels: {
+        job: service.workerHash,
+        experimentId: service.workRequest.experimentId,
+      },
+    },
+    spec: {
+      template: {
+        metadata: {
+          name: `job-${service.workerHash}-template`,
+          labels: {
+            job: service.workerHash,
+            experimentId: service.workRequest.experimentId,
+          },
+        },
+        spec: {
+          containers: [
+            {
+              name: `job-${service.workerHash}-container`,
+              image: imageUrl,
+              imagePullPolicy: 'Always',
+              env: [
+                {
+                  name: 'WORK_QUEUE',
+                  value: service.workQueueName,
+                },
+                {
+                  name: 'K8S_ENV',
+                  value: `${config.clusterEnv}`,
+                },
+              ],
+              volumeMounts: [
+                {
+                  name: 'data',
+                  mountPath: '/data',
+                },
+              ],
+            },
+          ],
+          volumes: [
+            {
+              name: 'data',
+              persistentVolumeClaim: {
+                claimName: `job-${service.workerHash}`,
+              },
+            },
+          ],
+          serviceAccountName: 'deployment-runner',
+          restartPolicy: 'OnFailure',
+        },
+      },
+    },
+  });
+};
+
+module.exports = createWorkerResources;
---
+++ src/api/general-services/work-submit/index.js
@@ -0,0 +1,111 @@
+const AWS = require('aws-sdk');
+const crypto = require('crypto');
+const createWorkerResources = require('./create-worker-k8s');
+const config = require('../../../config');
+const logger = require('../../../utils/logging');
+
+class WorkSubmitService {
+  constructor(workRequest) {
+    this.workRequest = workRequest;
+
+    this.workerHash = crypto
+      .createHash('sha1')
+      .update(this.workRequest.experimentId)
+      .digest('hex');
+
+    if (config.clusterEnv === 'development') {
+      this.workQueueName = 'development-queue.fifo';
+    } else {
+      this.workQueueName = `queue-job-${this.workerHash}-${config.clusterEnv}.fifo`;
+    }
+  }
+
+  /**
+   * Creates or locates an SQS queue for the appropriate
+   * worker.
+   */
+  async createQueue() {
+    const sqs = new AWS.SQS({
+      region: config.awsRegion,
+    });
+    const q = await sqs.createQueue({
+      QueueName: this.workQueueName,
+      Attributes: {
+        FifoQueue: 'true',
+        ContentBasedDeduplication: 'true',
+      },
+    }).promise();
+
+    const { QueueUrl: queueUrl } = q;
+
+    return queueUrl;
+  }
+
+  /**
+   * Returns a `Promise` to send an appropriately
+   * formatted task to the Job via an SQS queue.
+   * @param {string} queueUrl adsas
+   */
+  async sendMessageToQueue(queueUrl) {
+    logger.log(`Sending message to queue ${queueUrl}...`);
+    const sqs = new AWS.SQS({
+      region: config.awsRegion,
+    });
+
+    await sqs.sendMessage({
+      MessageBody: JSON.stringify(this.workRequest),
+      QueueUrl: queueUrl,
+      MessageGroupId: 'work',
+    }).promise();
+  }
+
+  async getQueueAndHandleMessage() {
+    if (config.clusterEnv === 'development') {
+      logger.log('In development, directly creating a queue...');
+      const queueUrl = await this.createQueue();
+      await this.sendMessageToQueue(queueUrl);
+      return 'success';
+    }
+
+    try {
+      const accountId = await config.awsAccountIdPromise();
+      const queueUrl = `https://sqs.${config.awsRegion}.amazonaws.com/${accountId}/${this.workQueueName}`;
+      await this.sendMessageToQueue(queueUrl);
+    } catch (error) {
+      if (error.code !== 'AWS.SimpleQueueService.NonExistentQueue') { throw error; }
+      const queueUrl = await this.createQueue();
+      await this.sendMessageToQueue(queueUrl);
+    }
+    return 'success';
+  }
+
+  /**
+    * Launches a Kubernetes `Job` with the appropriate configuration.
+    */
+  async createWorker() {
+    if (config.clusterEnv === 'development') {
+      logger.log('Not creating a worker because we are running locally...');
+      return;
+    }
+
+
+    try {
+      await createWorkerResources(this);
+    } catch (error) {
+      if (error.statusCode !== 409) {
+        logger.trace(error);
+        logger.error(error.statusCode);
+        throw new Error(error);
+      }
+    }
+  }
+
+  async submitWork() {
+    await Promise.all([
+      this.createWorker(),
+      this.getQueueAndHandleMessage(),
+    ]);
+  }
+}
+
+module.exports = WorkSubmitService;
