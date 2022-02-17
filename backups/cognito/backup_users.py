de835c3109c7521ffb75365afc3a25a5b966dac3
stefanbabukov <stefanbabukov98@gmail.com>
2022-02-17 11:34:57 +0000

    change

--- backups/cognito/backup_users.py
+++ backups/cognito/backup_users.py
@@ -7,45 +7,59 @@ import os
 client = boto3.client('cognito-idp')
 s3_client = boto3.client('s3')
 
+environment='production'
+user_pool_name = 'biomage-user-pool-case-insensitive-{}'.format(environment)
+backup_bucket = 'biomage-backups-{}'.format(environment)
+
 def backup_users():
     try:
         all_users = get_all_users()
+        num_users = len(all_users)
     except Exception as e:
-        print('Getting current cognito users has failed ', e)
+        raise Exception('Getting current cognito users has failed ', e)
 
-    csv_headers = ['cognito:username','name','given_name',
-    'family_name','middle_name','nickname','preferred_username',
-    'profile','picture','website','email','email_verified',
+    csv_headers = ['cognito:username','name','email','email_verified',
+    'cognito:mfa_enabled','phone_number_verified',
+    'given_name','family_name','middle_name','nickname','preferred_username',
+    'profile','picture','website',
     'gender','birthdate','zoneinfo','locale','phone_number',
-    'phone_number_verified','address','updated_at','cognito:mfa_enabled']
+    'address','updated_at', 'custom:institution']
     
     with open('data.csv','w', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerow(csv_headers)
+        lines_written = 0
 
         for user in all_users:
             name = list(filter(lambda attribute: attribute['Name']=='name', user['Attributes']))[0]['Value']
             email = list(filter(lambda attribute: attribute['Name']=='email', user['Attributes']))[0]['Value']
             email_verified = list(filter(lambda attribute: attribute['Name']=='email_verified', user['Attributes']))[0]['Value']
             
-            new_row = [email,name,'','','','','','','','',email,email_verified,'','','','','','FALSE','','','FALSE']
+            new_row = [email,name,email,email_verified,'FALSE','FALSE','','','','','','','','','','','','','','','']
             writer.writerow(new_row)
-            file_name = '{}/biomage-userpool-case-insensitive-production/data.csv'.format(datetime.now().isoformat())
+            lines_written += 1
+        
+        if lines_written != num_users:
+            raise Exception('Number of lines written does not match number of users in cognito')
+
     try:
-        s3_client.upload_file('data.csv', 'biomage-backups-production', file_name)
+        file_name = '{}/{}/data.csv'.format(datetime.now().isoformat(), user_pool_name)
+        s3_client.upload_file('data.csv', backup_bucket, file_name)
         #removing the local file after saving to S3
         os.remove('data.csv')
     except ClientError as e:
-        print('Uploading backup to S3 has failed ', e)
+        raise Exception('Uploading backup to S3 has failed ', e)
     
 def get_all_users():
-    cognito = boto3.client('cognito-idp')
-    
+    user_pools = client.list_user_pools(
+    MaxResults=10,   
+    )['UserPools']
+    user_pool_id = list(filter(lambda user_pool: user_pool['Name']==user_pool_name, user_pools))[0]['Id']
+
     users = []
     next_page = None
     kwargs = {
-        # users-insensitive-production pool
-        'UserPoolId': 'eu-west-1_eYTCV3Nl7',
+        'UserPoolId': user_pool_id,
         'AttributesToGet': ['email', 'email_verified', 'name']
     }
 
@@ -53,11 +67,10 @@ def get_all_users():
     while(users_remain):
         if next_page:
             kwargs['PaginationToken'] = next_page
-        response = cognito.list_users(**kwargs)
+        response = client.list_users(**kwargs)
         users.extend(response['Users'])
         next_page = response.get('PaginationToken', None)
         users_remain = next_page is not None
-
     return users
 
 backup_users()
