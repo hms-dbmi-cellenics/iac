5fc9a8d51d2b90737c19e6f48abb537d3609eb4a
cosa65 <martin@biomage.net>
2022-03-14 12:13:41 -0300

    Small fixes

--- migrations/sql-migrations/dynamoToSql.js
+++ migrations/sql-migrations/dynamoToSql.js
@@ -7,9 +7,9 @@ const knex = require('knex');
 
 
 // ----------------------Dynamo dumps----------------------
-const projects = require('projects');
-const experiments = require('experiments');
-const samples = require('samples');
+const projects = require('./downloaded_data/projects-production.json');
+const experiments = require('./downloaded_data/experiments-production.json');
+const samples = require('./downloaded_data/samples-production.json');
 // ----------------------Dynamo dumps END------------------
 
 const getSqlClient = async () => {
@@ -17,86 +17,93 @@ const getSqlClient = async () => {
   return knex.default(knexfile['staging']);
 }
 
-const run = async () => {
-  projects.forEach((project) => {
-    const { projectUuid } = project;
-    const projectData = project.projects[projectUuid];
-    
-    const experimentId = projectData.experiments[0];
-    const experimentData = _.find(experiments, { 'experimentId': experimentId });
-
-    const sampleData = _.find(samples, { 'experimentId': experimentId });
-
-    console.log(`Migrating ${projectUuid}, experiment ${experimentId}`);
-
-    if (_.isNil(projectData), _.isNil(experimentData), _.isNil(sampleData)) {
-      console.log(`[ ERROR ] - ${experimentId} - One of these is nil:`);
-      console.log(`projectData: ${projectData}, experimentData: ${experimentData}, sampleData: ${sampleData}`)
-      return;
-    }
-
-    // SQL tables we will need to upload into:
-    // 
-    //  dynamo experiments and projects:
-    //    - experiment
-    //    - experiment_execution
-    // 
-    //  dynamo samples:
-    //    - sample
-    //    – sample_file
-    //    - sample_to_sample_file_map
-    //    - metadata_track
-    //    - sample_in_metadata_track_map
-    // 
-    // Separate tables to insert into with their dynamo counterparts:
-    // - invite_access
-    // - user_access
-    // - plot
-    
-    const sqlClient = await getSqlClient();
+const migrateProject = async (project) => {
+  const { projectUuid, projects: projectData } = project;
   
-    const sqlExperiment = {
-      id: experimentId,
-      name: projectData.name,
-      description: projectData.description,
-      processing_config: experimentData.processingConfig,
-      created_at: projectData.createdDate,
-      updated_at: projectData.lastModified,
-      notify_by_email: experimentData.notifyByEmail,
+  console.log('projectDebug');
+  console.log(project);
+
+  const experimentId = projectData.experiments[0];
+  const experimentData = _.find(experiments, { 'experimentId': experimentId });
+
+  const sampleData = _.find(samples, { 'experimentId': experimentId });
+
+  console.log(`Migrating ${projectUuid}, experiment ${experimentId}`);
+
+  if (_.isNil(projectData), _.isNil(experimentData), _.isNil(sampleData)) {
+    console.log(`[ ERROR ] - ${experimentId} - One of these is nil:`);
+    console.log(`projectData: ${projectData}, experimentData: ${experimentData}, sampleData: ${sampleData}`)
+    return;
+  }
+
+  // SQL tables we will need to upload into:
+  // 
+  //  dynamo experiments and projects:
+  //    - experiment
+  //    - experiment_execution
+  // 
+  //  dynamo samples:
+  //    - sample
+  //    – sample_file
+  //    - sample_to_sample_file_map
+  //    - metadata_track
+  //    - sample_in_metadata_track_map
+  // 
+  // Separate tables to insert into with their dynamo counterparts:
+  // - invite_access
+  // - user_access
+  // - plot
+  
+  const sqlClient = await getSqlClient();
+
+  const sqlExperiment = {
+    id: experimentId,
+    name: projectData.name,
+    description: projectData.description,
+    processing_config: experimentData.processingConfig,
+    created_at: projectData.createdDate,
+    updated_at: projectData.lastModified,
+    notify_by_email: experimentData.notifyByEmail,
+  };
+
+  await sqlClient('experiment').insert(sqlExperiment);
+
+  // Create experiment executions if we need to
+
+  if (!_.isNil(experimentData.meta.gem2s)) {
+    const { paramsHash, stateMachineArn, executionArn } = experimentData.meta.gem2s;
+
+    const sqlExperimentExecution = {
+      experiment_id: experimentId,
+      pipeline_type: 'gem2s',
+      params_hash: paramsHash,
+      state_machine_arn: stateMachineArn,
+      execution_arn: executionArn,
     };
 
-    await sqlClient('experiment').insert(sqlExperiment);
+    await sqlClient('experiment_execution').insert(sqlExperimentExecution);
+  }
 
-    // Create experiment executions if we need to
+  if (!_.isNil(experimentData.meta.pipeline)) {
+    const { paramsHash, stateMachineArn, executionArn } = experimentData.meta.pipeline;
 
-    if (!_.isNil(experimentData.meta.gem2s)) {
-      const { paramsHash, stateMachineArn, executionArn } = experimentData.meta.gem2s;
+    const sqlExperimentExecution = {
+      experiment_id: experimentId,
+      pipeline_type: 'qc',
+      params_hash: paramsHash,
+      state_machine_arn: stateMachineArn,
+      execution_arn: executionArn,
+    };
 
-      const sqlExperimentExecution = {
-        experiment_id: experimentId,
-        pipeline_type: 'gem2s',
-        params_hash: paramsHash,
-        state_machine_arn: stateMachineArn,
-        execution_arn: executionArn,
-      };
+    await sqlClient('experiment_execution').insert(sqlExperimentExecution);
+  }
+}
 
-      await sqlClient('experiment_execution').insert(sqlExperimentExecution);
-    }
-
-    if (!_.isNil(experimentData.meta.pipeline)) {
-      const { paramsHash, stateMachineArn, executionArn } = experimentData.meta.pipeline;
-
-      const sqlExperimentExecution = {
-        experiment_id: experimentId,
-        pipeline_type: 'qc',
-        params_hash: paramsHash,
-        state_machine_arn: stateMachineArn,
-        execution_arn: executionArn,
-      };
-
-      await sqlClient('experiment_execution').insert(sqlExperimentExecution);
-    }
-  });
+const run = async () => {
+  migrateProject(projects[0]);
+  // projects.forEach(async (project) => {
+  //   await migrateProject(project);
+  // });
 };
 
 run()
