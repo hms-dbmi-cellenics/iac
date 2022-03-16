8b8c047e8a9e9235b7f8935a685371749ded317f
cosa65 <martin@biomage.net>
2022-03-16 15:33:13 -0300

    Set max connections 100 for migration running in prod or staging

--- migrations/sql-migrations/knexfile.js
+++ migrations/sql-migrations/knexfile.js
@@ -2,12 +2,14 @@ const getConnectionParams = require('./getConnectionParams');
 
 // This is one of the shapes the knexfile can take https://knexjs.org/#knexfile
 const fetchConfiguration = async (environment) => {
+  const maxConnections = environment === 'development' ? 10 : 100;
+
   const params = await getConnectionParams(environment);
   return {
     [environment]: {
       client: 'postgresql',
       connection: params,
-      pool: { min: 0, max: 100 },
+      pool: { min: 0, max: maxConnections },
       acquireConnectionTimeout: 6000000
     },
   };
@@ -15,7 +17,7 @@ const fetchConfiguration = async (environment) => {
 
 module.exports = async (env) => {
   const environment = env || process.env.NODE_ENV;
-  
+
   const configuration = await fetchConfiguration(environment);
   return {
     ...configuration,
