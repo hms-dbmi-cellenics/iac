383ac1b289b5e16fde4a7ded63917bbdc6889988
cosa65 <martin@biomage.net>
2022-05-31 11:54:04 +0100

    Add migration fro making processing config camelcased

---
+++ migrations/dynamodb-migrations/2022-05-31T11:46:17Z-make-processing-config-camelcased.js
@@ -0,0 +1,60 @@
+const Dyno = require('dyno');
+
+let updated = 0;
+
+// Camelcases absolute_threshold wherever it finds it
+const recursiveCamelcase = (processingConfig) => (
+  _.transform(processingConfig, (acc, value, key, target) => {
+    let camelKey;
+
+    if (key === 'absolute_threshold') {
+      camelKey = 'absoluteThreshold';
+    } else {
+      camelKey = key;
+    }
+
+    if (_.isObject(value)) {
+      acc[camelKey] = recursiveCamelcase(value);
+    } else {
+      let camelValue = value;
+      if(camelValue === 'absolute_threshold'){
+        camelValue = 'absoluteThreshold';
+      }
+  
+      acc[camelKey] = camelValue;
+    }
+  })
+);
+
+module.exports = (record, dyno, callback) => {
+  if (!process.env.K8S_ENV) {
+    throw new Error('Environment (staging/production) must be specified.');
+  }
+
+  const attrValues = {
+    ':pid': record.experimentId,
+    ':p_config': recursiveCamelcase(record.processingConfig)
+  };
+
+  // If you are running a dry-run, `dyno` will be null
+  if (!dyno) return callback();
+
+  dyno.updateItem({
+    Key: {experimentId: record.experimentId},
+    UpdateExpression: 'SET processingConfig = :p_config',
+    ExpressionAttributeValues: attrValues
+  }, (err) => {
+    if(err) {
+      console.log(`${record.experimentId} failed to update`);
+      throw new Error(err);
+    }
+  });
+
+  updated++;
+  callback();
+}
+
+module.exports.finish = (dyno, callback) => {
+  console.log(`Updated ${updated} records.`);
+  callback();
+}
