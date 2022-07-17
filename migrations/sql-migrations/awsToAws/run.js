556849b2a6b5d50c0e01104adc40efe0bbdaa293
Alex Pickering <alexvpickering@gmail.com>
2022-07-17 11:25:04 -0700

    connection and example table queries working

--- migrations/sql-migrations/awsToAws/run.js
+++ migrations/sql-migrations/awsToAws/run.js
@@ -5,9 +5,6 @@ const knex = require('knex');
 const USER_POOL_ID = 'eu-west-1_eYTCV3Nl7'
 const DOWNLOAD_FOLDER = '../cognito_backup'
 
-// TODO: adapt Helper
-const Helper = require('./Helper');
-
 const env = process.env.SOURCE_ENV || 'production'
 
 // Cognito dumps 
@@ -25,35 +22,78 @@ const createSqlClient = async (activeEnvironment, sandboxId) => {
   return knex.default(knexfile[activeEnvironment]);
 }
 
+const migrateUser = async (user, sqlClient) => {
+  const { sub: userId, email } = user;
+
+  console.log(`Migrating User:`)
+  console.log(JSON.stringify(user));
+
+  // get all experiment_id's for user
+  const userAccessEntries = await sqlClient('user_access')
+    .where('user_id', userId);
+
+  // ----
+  // TODO: update user_id to be for destination deployment
+  // TODO: insert entries into user_acess table
+  // ----
+
+  // console.log('userAccessEntries:')
+  // console.log(userAccessEntries)
+
+  // migrate each experiment
+  for (var i = 0; i < userAccessEntries.length; i++) {
+    await migrateExperiment(userAccessEntries[i], sqlClient);
+  }
+};
+
+const migrateExperiment = async (experiment, sqlClient) => {
+
+  // get entries from experiment table
+  const { experiment_id: experimentId } = experiment;
+
+  console.log(`Migrating experiment:`)
+  console.log(JSON.stringify(experiment));
+
+  const experimentTableEntries = await sqlClient('experiment')
+    .where('id', experimentId);
+
+
+  console.log('experimentTableEntries:')
+  console.log(experimentTableEntries)
+
+};
+
 const run = async (environment, sandboxId) => {
   // where users will be migrated from
-  // const sourceSqlClient = await createSqlClient(environment);
+  const sqlClient = await createSqlClient(environment, sandboxId);
 
+  // TODO: create destinationSqlClient (aka knex for HMS deployment)
 
-  // TODO: create destination destinationSqlClient
+  // subset to users that will migrate
+  const usersToMigrate = cognitoUsersJson
+    .filter(user => {
+      const emailAttribute = user.Attributes.filter(attr => attr.Name == "email");
+      return emailsToMigrate.includes(emailAttribute[0].Value);
+    })
+    // only keep attributes: have everything we need and are flat
+    .map(user => {
+      const flatAttributes = {};
+      user.Attributes.forEach(attr => flatAttributes[attr.Name] = attr.Value);
+      return flatAttributes;
+    })
 
-  const usersToMigrate = cognitoUsersJson.filter(user => {
-    const emailAttribute = user.Attributes.filter(attr => attr.Name == "email");
-    return emailsToMigrate.includes(emailAttribute[0].Value);
-  })
+  // migrate each user
+  for (var i = 0; i < usersToMigrate.length; i++) {
+    await migrateUser(usersToMigrate[i], sqlClient);
+  };
 
-  console.log(`usersToMigrate:`)
-  console.log(JSON.stringify(usersToMigrate))
-
-
-  // const helper = new Helper(sqlClient);
-
-  // await migrateProjects(projectsJson, helper);
-
-  // await Promise.all([
-  //   migrateUserAccess(sqlClient, userAccessJson),
-  //   migrateInviteAccess(sqlClient, inviteAccessJson),
-  //   migratePlots(sqlClient, plotsJson)
-  // ]);
 };
 
 const environment = process.env.TARGET_ENV || 'development';
 const sandboxId = process.env.SANDBOX_ID || 'default';
+
+console.log(`environment: ${environment}`)
+console.log(`sandboxId: ${sandboxId}`)
 
 if (!_.isNil(environment)) {
   run(environment, sandboxId)
