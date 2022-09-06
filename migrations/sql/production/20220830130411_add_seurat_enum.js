77053286d4ee5ca68b5a8d3173e85f61c15e5333
Alex Pickering <alexvpickering@gmail.com>
2022-09-06 10:59:47 -0700

    add sample_file_type to migration

--- migrations/sql/production/20220830130411_add_seurat_enum.js
+++ migrations/sql/production/20220830130411_add_seurat_enum.js
@@ -6,6 +6,13 @@ exports.up = async (knex) => {
       ALTER COLUMN sample_technology TYPE sample_technology_temp USING sample_technology::text::sample_technology_temp;
     DROP TYPE IF EXISTS sample_technology;
     ALTER TYPE sample_technology_temp RENAME TO sample_technology;
+
+    CREATE TYPE sample_file_type_temp AS ENUM ('features10x','barcodes10x','matrix10x', 'rhapsody', 'seurat');
+    ALTER TABLE sample_file
+      ALTER COLUMN sample_file_type DROP DEFAULT,
+      ALTER COLUMN sample_file_type TYPE sample_file_type_temp USING sample_file_type::text::sample_file_type_temp;
+    DROP TYPE IF EXISTS sample_file_type;
+    ALTER TYPE sample_file_type_temp RENAME TO sample_file_type;
   `);
 };
 
@@ -17,6 +24,13 @@ exports.down = async (knex) => {
       ALTER COLUMN sample_technology TYPE sample_technology_temp USING sample_technology::text::sample_technology_temp;
     DROP TYPE IF EXISTS sample_technology;
     ALTER TYPE sample_technology_temp RENAME TO sample_technology;
+
+    CREATE TYPE sample_file_type_temp AS ENUM ('features10x','barcodes10x','matrix10x', 'rhapsody');
+    ALTER TABLE sample_file
+      ALTER COLUMN sample_file_type DROP DEFAULT,
+      ALTER COLUMN sample_file_type TYPE sample_file_type_temp USING sample_file_type::text::sample_file_type_temp;
+    DROP TYPE IF EXISTS sample_file_type;
+    ALTER TYPE sample_file_type_temp RENAME TO sample_file_type;
   `);
 };
 
