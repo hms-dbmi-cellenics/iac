a9c2a39c023f20c56c7f1b951d3db54217bbf655
cosa65 <martin@biomage.net>
2022-09-30 13:38:07 -0300

    Remove commented out code

--- migrations/s3-migrations/2022-09-29-migrate-cellsets-samples/migrate-single-experiment.sh
+++ migrations/s3-migrations/2022-09-29-migrate-cellsets-samples/migrate-single-experiment.sh
@@ -36,33 +36,5 @@ biomage experiment upload \
 
 echo "[INFO] Successfully patched ${experiment_id}" | tee -a ${migration_log_file}
 
-# ## 6. Migrate state machines dates and remove state machine for qc
-# state_machine_arn=$(echo "select state_machine_arn
-# from experiment_execution ee
-# where experiment_id = '${experiment_id}' and "pipeline_type" = 'qc'"  |
-#       biomage rds run -i ${MIGRATION_ENV} psql |
-#       grep 'arn:' |
-#       xargs)
-
-# aws stepfunctions delete-state-machine --state-machine-arn ${state_machine_arn}
-# echo "[INFO] Deleted state machine ${state_machine_arn} for ${experiment_id}"  | tee -a ${migration_log_file}
-
-
-# echo "UPDATE experiment_execution SET
-# last_status_response = jsonb_set(
-#     last_status_response,
-#     '{qc}', last_status_response->'qc' || '{\"startDate\": \"2022-09-26T00:00:00.000Z\", \"stopDate\": \"2022-09-26T00:00:00.000Z\"}')
-# where experiment_id = '${experiment_id}' and pipeline_type = 'qc'"  |
-#       biomage rds run -i ${MIGRATION_ENV} psql
-
-# echo "[INFO] Migrated QC to new date for ${experiment_id}"  | tee -a ${migration_log_file}
-
-# echo "UPDATE experiment_execution SET
-# last_status_response = jsonb_set(
-#     last_status_response,
-#     '{gem2s}', last_status_response->'gem2s' || '{\"startDate\": \"2022-09-26T00:00:00.000Z\", \"stopDate\": \"2022-09-26T00:00:00.000Z\"}')
-# where experiment_id = '${experiment_id}' and pipeline_type = 'gem2s'"  |
-#       biomage rds run -i ${MIGRATION_ENV} psql
-
 echo "[OK] Migrated ${experiment_id}"  | tee -a ${migration_log_file}
 
