fc3e7900f47554a542b0ee8a2a4091dc2585e7b4
biomage-org <ci@biomage.net>
2023-01-17 07:36:44 +0000

    Update from https://github.com/biomage-org/api/commit/113145ecacadac215128d85ddc60569e503c13a5

--- migrations/sql/staging/20230111112324_unique-sample-names.js
+++ migrations/sql/staging/20230111112324_unique-sample-names.js
@@ -1,8 +1,8 @@
 
-exports.up = async function (knex) {
+exports.up = async (knex) => {
   await knex.raw('ALTER TABLE sample ADD CONSTRAINT unique_sample_names UNIQUE (name, experiment_id);');
 };
 
-exports.down = async function (knex) {
+exports.down = async (knex) => {
   await knex.raw('ALTER TABLE sample DROP CONSTRAINT unique_sample_names;');
 };
