a470c921a26330ef5048b0021e7991360899a8c6
Alex Pickering <alexvpickering@gmail.com>
2023-03-27 09:35:41 -0700

    try rename last_gem2s_params
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- migrations/sql/production/20230117130009_add_last_gem2s_params.js
+++ migrations/sql/production/20230117130009_add_last_gem2s_params.js
@@ -1,11 +1,11 @@
 exports.up = async (knex) => {
   await knex.schema.alterTable('experiment_execution', (table) => {
-    table.jsonb('last_pipeline_params').nullable().defaultTo(null);
+    table.jsonb('last_gem2s_params').nullable().defaultTo(null);
   });
 };
 
 exports.down = async (knex) => {
   await knex.schema.alterTable('experiment_execution', (table) => {
-    table.dropColumn('last_pipeline_params');
+    table.dropColumn('last_gem2s_params');
   });
 };
