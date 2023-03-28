990894829f0ddee424a46fd414e43972fd8ca5b9
hms-dbmi-cellenics <ci@biomage.net>
2023-03-28 17:52:34 +0000

    Update from https://github.com/hms-dbmi-cellenics/api/commit/75c6819c17f6241ccca6dca5c89ffad784b34b84

--- migrations/sql/staging/20230117130009_add_last_pipeline_params.js
+++ migrations/sql/staging/20230117130009_add_last_pipeline_params.js
@@ -1,11 +1,11 @@
 exports.up = async (knex) => {
   await knex.schema.alterTable('experiment_execution', (table) => {
-    table.jsonb('last_pipeline_params').nullable().defaultTo(null);
+    table.renameColumn('last_gem2s_params', 'last_pipeline_params');
   });
 };
 
 exports.down = async (knex) => {
   await knex.schema.alterTable('experiment_execution', (table) => {
-    table.dropColumn('last_pipeline_params');
+    table.renameColumn('last_pipeline_params', 'last_gem2s_params');
   });
 };
---
+++ migrations/sql/staging/20230303140501_delete_params_hash_field.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  await knex.schema.alterTable('experiment_execution', (table) => {
+    table.dropColumn('params_hash');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.alterTable('experiment_execution', (table) => {
+    table.string('params_hash').nullable();
+  });
+};
