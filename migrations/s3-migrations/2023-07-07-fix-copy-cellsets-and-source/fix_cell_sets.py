2d0c2bcee257415c96ad6731933052d2e533a6e9
cosa65 <martin@biomage.net>
2023-07-27 09:32:15 -0300

    Add comment

--- migrations/s3-migrations/2023-07-07-fix-copy-cellsets-and-source/fix_cell_sets.py
+++ migrations/s3-migrations/2023-07-07-fix-copy-cellsets-and-source/fix_cell_sets.py
@@ -12,8 +12,15 @@ cs = json.load(open(in_file_path))
 
 set_ids = set()
 counts = Counter()
+
 for cell_class in cs['cellSets']:
     for cell_set in cell_class['children']:
+        # Check, for any cellSet that is child:
+        # if cell_set["rootNode"] is set to None (which breaks ScType), 
+        #   then set it to False instead.
+        # if cell_set["type"] is set to None (which breaks ScType), 
+        #   then set it to the type of its parent instead, 
+        #   the type in the parent is the same as the one in the child.
         if (cell_set["rootNode"] == None):
             cell_set["rootNode"] = False
         if (cell_set["type"] == None):
