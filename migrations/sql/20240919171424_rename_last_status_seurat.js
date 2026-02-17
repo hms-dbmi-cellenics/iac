513cdf60cd474a60217138d02a818866da5ce8f2
hms-dbmi-cellenics <ci@biomage.net>
2026-02-17 20:18:14 +0000

    Update from https://github.com/hms-dbmi-cellenics/api/commit/dc6b7cba29690ed132f59aebe9c87ceac035e303

--- migrations/sql/20240919171424_rename_last_status_seurat.js
+++ migrations/sql/20240919171424_rename_last_status_seurat.js
@@ -5,14 +5,15 @@
 exports.up = async (knex) => {
   // We're using `pipeline_type::text = 'obj2s'` to cast the enum to a text type.
   // This avoids the potential issue of "unsafe use of new value" caused by enum strictness.
+  const sqlStatement = 'jsonb_set(last_status_response::jsonb, \'{obj2s}\', '
+    + 'last_status_response::jsonb->\'seurat\', true) '
+    + '- \'seurat\'';
   await knex('experiment_execution')
     .where(knex.raw("pipeline_type::text = 'obj2s'")) // Cast enum to text for comparison
     .update({
-      // Use jsonb_set to add the new `obj2s` key to last_status_response with the value from `seurat`.
-      // Then, remove the old `seurat` key from last_status_response.
-      last_status_response: knex.raw(
-        'jsonb_set(last_status_response::jsonb, \'{obj2s}\', last_status_response::jsonb->\'seurat\', true) - \'seurat\'',
-      ),
+      // Use jsonb_set to add the new `obj2s` key to last_status_response
+      // with the value from `seurat` and remove the old `seurat` key.
+      last_status_response: knex.raw(sqlStatement),
     });
 };
 
